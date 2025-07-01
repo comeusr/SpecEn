e837100e697ddfb89cf90e02d1db57d9da5decaa
Ziyi Wang <wang4538@purdue.edu>
2025-07-01 23:25:44 +0000

    Remove HF token

--- run_sft.sh
+++ run_sft.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
-export HF_DATASETS_OFFLINE=1
-export HF_HUB_OFFLINE=1
+export HF_TOKEN=holder
+export WANDB_API_KEY=holder
+
+export MASTER_ADDR=127.0.0.1
+export MASTER_PORT=$(shuf -i 29500-29600 -n 1)
+
+wandb login $WANDB_API_KEY
+huggingface-cli login --token $HF_TOKEN
 
 accelerate launch \
-    --config_file accelerate_config/fsdp_8gpu.yaml \
-    launch.py loss=sft model=llama datasets=[gsm8k] exp_name=llama3-8B-sft \
+    --config_file accelerate_config/fsdp_4gpu.yaml \
+    --main_process_port $MASTER_PORT \
+    sft_train.py loss=sft model=qwen datasets=[gsm8k] exp_name=Qwen3-8B_Qwen-06B_sft \
+    wandb.project=Ensemble \
+    model.name_or_path="Qwen/Qwen3-8B" \
+    model.use_peft=false lr=1e-6 model.batch_size=4
+    
+    
